05a1f7c68051b624f32a5049e189e0b8ff5a6ba6
Macy Kay Mannix <macymannix@gmail.com>
2022-04-16 20:18:44 -0700

    java

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+const startButton = document.getElementById("start-btn")
+const questionContainerElement = document.getElementById
+ ('question-container')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('Started')
+    startButton.classList.add('hide')
+}
+
+function setNextQuestion() {
+
+}
+
+function selectAnswer() {
+
+}
+
